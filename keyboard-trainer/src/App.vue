8ed78e773f75ca86cc649a74df14e8c53e1f3996
iri.sobakina <iri.dolgopolova@gmail.com>
2023-04-07 00:21:43 +0300

    Добавляет интерактивную клавиатуру и пальчики

--- keyboard-trainer/src/App.vue
+++ keyboard-trainer/src/App.vue
@@ -36,13 +36,26 @@
         <div class="container">
             <div class="task-box">
                 <p class="task-box__text">
-                    {{ text }}
+                    <span class="task-box__text__correctly">
+                       {{ correctlyLetters.join('') }}
+                    </span>
+                    <span class="task-box__text__current">
+                        {{ currentLetter }}
+                    </span>
+                    <span>
+                       {{ letters.join('') }}
+                    </span>
                 </p>
             </div>
 
             <div class="interactive-box">
                 <div class="interactive-box__left-hand">
-                    <leftEmpty />
+                    <leftEmpty v-if="isLeftEmpty" />
+                    <leftLittle v-if="currentFinger === 'left_5'"/>
+                    <leftRing v-if="currentFinger === 'left_4'"/>
+                    <leftMiddle v-if="currentFinger === 'left_3'"/>
+                    <leftForefinger v-if="currentFinger === 'left_2'"/>
+                    <leftThumb v-if="currentFinger === 'left_1'"/>
                 </div>
 
                 <div class="interactive-box__keyboard">
@@ -50,9 +63,19 @@
                         v-for="(keys, index) in arrayKey" :key="index"
                         class="interactive-box__keyboard__row"
                     >
+                    <!-- вынести в функции -->
                         <div
                             v-for="(key, index) in keys" :key="index"
-                            class="interactive-box__keyboard__key"
+                            :class="[
+                                'interactive-box__keyboard__key',
+                                {'space' : index === 'space'},
+                                {'pink' : (key.hand === 'left' && key.finger === 5) || (key.hand === 'right' && key.finger === 2)},
+                                {'yellow' : (key.hand === 'left' && key.finger === 4) || (key.hand === 'right' && key.finger === 3)},
+                                {'green' : (key.hand === 'left' && key.finger === 3) || (key.hand === 'right' && key.finger === 4)},
+                                {'blue' : (key.hand === 'left' && key.finger === 2) || (key.hand === 'right' && key.finger === 5)},
+                                {'brown' : (key.hand === 'left' && key.finger === 1) || (key.hand === 'right' && key.finger === 1)},
+                                {'active' : isCurrent(index, key)}
+                            ]"
                         >
                             {{ index }}
                         </div>
@@ -60,7 +83,12 @@
                 </div>
 
                 <div class="interactive-box__right-hand">
-                    <rightEmpty />
+                    <rightEmpty v-if="isRightEmpty"/>
+                    <rightLittle v-if="currentFinger === 'right_5'"/>
+                    <rightRing v-if="currentFinger === 'right_4'"/>
+                    <rightMiddle v-if="currentFinger === 'right_3'"/>
+                    <rightForefinger v-if="currentFinger === 'right_2'"/>
+                    <rightThumb v-if="currentFinger === 'right_1'"/>
                 </div>
             </div>
         </div>
@@ -77,6 +105,18 @@ import errorsIcon from "./components/icons/errorsIcon.vue"
 import leftEmpty from "./components/icons/hands/leftEmpty.vue"
 import rightEmpty from "./components/icons/hands/rightEmpty.vue"
 
+import leftLittle from "./components/icons/hands/leftLittle.vue"
+import leftRing from "./components/icons/hands/leftRing.vue"
+import leftMiddle from "./components/icons/hands/leftMiddle.vue"
+import leftForefinger from "./components/icons/hands/leftForefinger.vue"
+import leftThumb from "./components/icons/hands/leftThumb.vue"
+
+import rightLittle from "./components/icons/hands/rightLittle.vue"
+import rightRing from "./components/icons/hands/rightRing.vue"
+import rightMiddle from "./components/icons/hands/rightMiddle.vue"
+import rightForefinger from "./components/icons/hands/rightForefinger.vue"
+import rightThumb from "./components/icons/hands/rightThumb.vue"
+
 import arrayKey from "./components/keyboard/listKeys.json"
 
 export default {
@@ -84,38 +124,84 @@ export default {
     speedIcon,
     errorsIcon,
     leftEmpty,
-    rightEmpty
+    rightEmpty,
+    leftLittle,
+    leftRing,
+    leftMiddle,
+    leftForefinger,
+    leftThumb,
+    rightLittle,
+    rightRing,
+    rightMiddle,
+    rightForefinger,
+    rightThumb
   },
   data() {
     return {
-        text: "",
+        correctlyLetters: [],
+        letters: [],
+        currentLetter: '',
+        currentFinger: 0,
         arrayKey: arrayKey,
+        fingers: {
+            5: 'Litle',
+            4: 'Litle',
+            3: 'Litle',
+            2: 'Litle',
+            1: 'Litle',
+        }
+    }
+  },
+  computed: {
+    isLeftEmpty() {
+        return this.currentFinger.match(/right/i);
+    },
+    isRightEmpty() {
+        return this.currentFinger.match(/left/i);
     }
   },
   methods: {
     getTextData() {
-        fetch("https://fish-text.ru/get", {
-            type:"sentence",
-            number:1,
-            format:"json"
-        })
+        fetch("https://fish-text.ru/get?number=1")
             .then(responce => responce.json())
             .then(data => {
                 if (data.status == "success") {
-                    this.text = data.text
+                    // Разделили полученую строку на символы
+                    this.letters = data.text.split('')
+                    // выбираем первый элемент массива
+                    this.currentLetter = this.letters.shift()
                 } else {
                     console.log('error', data.status)
                 }
             })
             .catch(error => console.log('error', error))
+    },
+    test(e) {
+        if (e.key === this.currentLetter) {
+            this.correctlyLetters.push(this.currentLetter)
+            this.currentLetter = this.letters.shift()
+        }
+        // Добавить обработку неправильного нажатия
+    },
+    isCurrent(key, keyData) {
+        if (key === 'space') {
+            key = ' '
+        }
 
+        let isCurrentKey = (key === this.currentLetter.toLowerCase())
+
+        if (isCurrentKey) {
+            this.currentFinger = `${keyData.hand}_${keyData.finger}`
+        }
+
+        return isCurrentKey
     }
   },
   created() {
     this.getTextData()
   },
   mounted() {
-    console.log('arrayKey', arrayKey)
+    document.addEventListener('keyup', this.test)
   }
 }
 </script>
@@ -195,6 +281,14 @@ export default {
 
     &__text {
         @extend .text;
+
+        &__correctly {
+            @extend .text__correctly;
+        }
+
+        &__current {
+            @extend .text__current;
+        }
     }
 }
 
@@ -224,7 +318,7 @@ export default {
         &__row {
             display: flex;
             flex-direction: row;
-            justify-content: space-between;
+            justify-content: center;
 
             width: 100%;
         }
@@ -236,9 +330,38 @@ export default {
             margin: 8px 4px;
 
             width: 100%;
+            max-width: 80px;
 
             border: 1px solid $c-gray;
             border-radius: $border-radius;
+        }
+
+        .space {
+            max-width: 520px;
+        }
+
+        .pink {
+            background-color: $c-pink;
+        }
+
+        .yellow {
+            background-color: $c-yellow;
+        }
+
+        .brown {
+            background-color: $c-brown;
+        }
+
+        .green {
+            background-color: $c-green;
+        }
+
+        .blue {
+            background-color: $c-blue;
+        }
+
+        .active {
+            outline: 3px solid $c-red;
         }
     }
 }
